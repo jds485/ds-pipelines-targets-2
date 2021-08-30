7647b83ac95bd766c8a07ebc145543bf02638f1d
Smith <jsmith@usgs.gov>
2021-08-30 11:45:36 -0400

    Made separate targets for each of the downloaded files

--- _targets.R
+++ _targets.R
@@ -6,10 +6,43 @@ source("3_visualize/src/plot_timeseries.R")
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval")) # Loading tidyverse because we need dplyr, ggplot2, readr, stringr, and purrr
 
+# Vector of stations for which to download data
+site_nums <- c("01427207", "01432160", "01435000", "01436690", "01466500")
+
 p1_targets_list <- list(
   tar_target(
+    # create the file names that are needed for download_nwis_site_data
+    site_download_paths, 
+    file.path("1_fetch/out", paste0('nwis_', site_nums, '_data.csv'))
+  ),
+  tar_target(
+    station1_csv,
+    download_nwis_site_data(filepath = site_download_paths[1]),
+    format = "file"
+  ),
+  tar_target(
+    station2_csv,
+    download_nwis_site_data(filepath = site_download_paths[2]),
+    format = "file"
+  ),
+  tar_target(
+    station3_csv,
+    download_nwis_site_data(filepath = site_download_paths[3]),
+    format = "file"
+  ),
+  tar_target(
+    station4_csv,
+    download_nwis_site_data(filepath = site_download_paths[4]),
+    format = "file"
+  ),
+  tar_target(
+    station5_csv,
+    download_nwis_site_data(filepath = site_download_paths[5]),
+    format = "file"
+  ),
+  tar_target(
     site_data,
-    download_nwis_data(),
+    combine_nwis_data(c(station1_csv, station2_csv, station3_csv, station4_csv, station5_csv))
   ),
   tar_target(
     site_info_csv,
